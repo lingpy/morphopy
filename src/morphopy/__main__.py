88d39c86c75a1376e42a5dc007bb74d583bf908f
lingulist <mattis.list@lingpy.org>
2019-03-04 19:38:38 +0100

    update

--- src/morphopy/__main__.py
+++ src/morphopy/__main__.py
@@ -2,6 +2,8 @@ from sys import argv
 from lingpy import *
 from lingpy import basictypes as bt
 from tabulate import tabulate
+import networkx as nx
+from collections import defaultdict
 
 def check_length(wordlist, columns):
     
@@ -35,13 +37,50 @@ def check_length(wordlist, columns):
                 values += [len(data)]
             forms += [data]
         if len(set(values)) != 1:
-            print('Problems in {0} ({1}, «{2}»):'.format(idx, wordlist[idx,
+            print('## Problems in {0} ({1}, «{2}»):'.format(idx, wordlist[idx,
                 'doculect'], wordlist[idx, 'concept']))
             table = []
             for f, s, c in zip(forms, values, columns):
                 table += [[c, f, s]]
-            print(tabulate(table, headers=['column', 'value', 'length']))
-            input('press ENTER to continue')
+            print(tabulate(table, headers=['column', 'value', 'length'],
+                tablefmt='pipe'))
+            print('')
+            #input('press ENTER to continue\n')
+
+def word_families(wordlist, morphemes='morphemes'):
+    
+    wf = defaultdict(list)
+    for idx, tokens, morphemes, rootids in wordlist.iter_rows('tokens',
+            'morphemes', 'rootids'):
+        tokens, morphemes, rootids = (
+                bt.lists(tokens), bt.strings(morphemes), bt.ints(rootids)
+                )
+        wordlist[idx, 'tokens'] = tokens
+        wordlist[idx, 'morphemes'] = morphemes
+        wordlist[idx, 'rootids'] = rootids
+
+        for tok, morph, rootid in zip(tokens.n, morphemes, rootids):
+            if not morph.startswith('_'):
+                wf[rootid] += [(idx, str(tok), morph)]
+
+    for rootid, vals in sorted(wf.items(), key=lambda x: len(x[1])):
+        
+        print('# ROOT {0}'.format(rootid))
+        table = []
+        for (idx, tok, morph) in sorted(vals, key=lambda x: x[1]):
+            table += [[
+                idx, 
+                wordlist[idx, 'doculect'], 
+                wordlist[idx, 'concept'],
+                tok,
+                morph,
+                wordlist[idx, 'tokens'], 
+                wordlist[idx, 'morphemes'],
+                ]]
+        print(tabulate(table, headers=['ID', 'Doculect', 'Concept', 'RootForm',
+            'RootConcept', 'Tokens', 
+            'Morphemes'], tablefmt='pipe'))
+        print('')
     
 
 def main():
@@ -51,3 +90,8 @@ def main():
         wordlist = Wordlist(argv[clidx])
         columns = argv[clidx+1:]
         check_length(wordlist, columns)
+
+    if 'word-families' in argv:
+        clidx = argv.index('word-families')+1
+        wordlist = Wordlist(argv[clidx])
+        word_families(wordlist)
