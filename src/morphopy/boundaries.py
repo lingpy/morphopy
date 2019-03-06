cb617f5f78e43153d58932f624389d0270492dd7
lingulist <mattis.list@lingulist.de>
2019-03-06 10:09:20 +0100

    update morphopy

--- src/morphopy/boundaries.py
+++ src/morphopy/boundaries.py
@@ -5,31 +5,36 @@ from lingpy import basictypes as bt
 from segments import tokenizer, profile
 from tabulate import tabulate
 
-def get_init_enit(tokens):
+def get_init_enit(tokens, debug=False):
     tokens = [t for t in tokens if t != '+']
 
-    #inits[str(sybs.n[0])] += 1
-    #enits[str(sybs.n[-1])] += 1
     prostring = prosodic_string(tokens)
     if 'X' in prostring:
         inidx = prostring.index('X')
     elif 'Y' in prostring and not 'X' in prostring:
         inidx = prostring.index('Y')
+    else:
+        inidx = 0
+        
     if 'Z' in prostring:
         enidx = prostring[::-1].index('Z')
     else:
         if 'Y' in prostring:
             enidx = prostring[::-1].index('Y')
-        else:
+        elif 'X' in prostring:
             enidx = prostring[::-1].index('X')
+        else:
+            enidx = 0
 
-    init, enit = tokens[:inidx], tokens[-(enidx):]
-    print(' '.join(tokens))
-    print(' '.join(list(prostring)))
-    print(' '.join(init)+' +++ '+' '.join(enit))
+    init, enit = tokens[:inidx], tokens[-(enidx+1):]
+    if debug:
+        print(' '.join(tokens))
+        print(' '.join(list(prostring)))
+        print(' '.join(init)+' +++ '+' '.join(enit))
+        input()
     return init, enit
 
-def get_boundaries(wordlist, language, segments='tokens'):
+def get_boundaries(wordlist, language, segments='tokens', threshold=0):
     
     inits = defaultdict(int)
     enits = defaultdict(int)
@@ -37,11 +42,16 @@ def get_boundaries(wordlist, language, segments='tokens'):
     
     for tokens in wordlist.get_list(col=language, entry=segments, flat=True):
         sybs = bt.lists(syllabify(tokens))
+        tokens = [{'_': '+', '#': '+'}.get(t, t) for t in tokens]
 
-        if init:
-            inits[' '.join(init)] += 1
-        if enit:
-            enits[' '.join(enit)] += 1
+        tokensplit = ' '.join(tokens).split(' + ')
+        for token in tokensplit:
+        
+            init, enit = get_init_enit(token.split())
+            if init:
+                inits[' '.join(init)] += 1
+            if enit:
+                enits[' '.join(enit)] += 1
 
         #print(tokens, prostring, init, enit, inidx, enidx)
         for segment in tokens:
@@ -49,20 +59,22 @@ def get_boundaries(wordlist, language, segments='tokens'):
 
     dpoints = []
     for key, vals in inits.items():
-        dpoints += [{'Grapheme': key.replace(' ', ''), 
-            'Segments': '← '+key,
-            'Frequency': vals}]
-        dpoints += [{'Grapheme': '^'+key.replace(' ', ''), 
-            'Segments': key,
-            'Frequency': vals}]
+        if vals > threshold:
+            dpoints += [{'Grapheme': key.replace(' ', ''), 
+                'Segments': '← '+key,
+                'Frequency': vals}]
+            dpoints += [{'Grapheme': '^'+key.replace(' ', ''), 
+                'Segments': key,
+                'Frequency': vals}]
 
     for key, vals in enits.items():
-        dpoints += [{'Grapheme': key.replace(' ', ''), 
-            'Segments': key+' →',
-            'Frequency': vals}]
-        dpoints += [{'Grapheme': key.replace(' ', '')+'$', 
-            'Segments': key,
-            'Frequency': vals}]
+        if vals > threshold:
+            dpoints += [{'Grapheme': key.replace(' ', ''), 
+                'Segments': key+' →',
+                'Frequency': vals}]
+            dpoints += [{'Grapheme': key.replace(' ', '')+'$', 
+                'Segments': key,
+                'Frequency': vals}]
 
     for segment, vals in segm.items():
         dpoints += [{'Grapheme': segment, 
@@ -74,12 +86,45 @@ def get_boundaries(wordlist, language, segments='tokens'):
     prf = tokenizer.Tokenizer(profile.Profile(*dpoints))
     
     table = []
-    for word in wordlist.get_list(col=language, entry=segments, flat=True):
-        tok = ''.join([t for t in word if t != '+'])
-        segs = prf('^'+tok+'$', column='Segments')
-        #print(' '.join(word), segs)
-        table += [[''.join(word), segs, prosodic_string(word)]]
+    morphemes = defaultdict(list)
+    S = {}
+    for idx, tokens in zip(
+            wordlist.get_list(col=language, flat=True),
+            wordlist.get_list(col=language, entry=segments, flat=True)):
+        tokens = [{'_': '+', '#': '+'}.get(t, t) for t in tokens]
+        segmented, segmented2 = [], []
+        for token in ' '.join(tokens).split(' + '):
+            tok = ''.join([t for t in token.split() if t != '+'])
+            segs = prf('^'+tok+'$', column='Segments')
+            #print(' '.join(word), segs)
+            sgs = segs.replace(' → ← ', ' + ')
+            sgs = sgs.replace(' → ', ' + ')
+            sgs = sgs.replace(' ← ', ' + ')
+            segmented += [sgs]
+            segmented2 += [segs]
+        
+        sgs = ' + '.join(segmented)
+        table += [[''.join(tokens), sgs, prosodic_string(tokens)]]
+        morphemes_ = sgs.split(' + ')
+        morphemes['^'+morphemes_[0]] += [idx]
+        morphemes[morphemes_[-1]+'$'] += [idx]
+        for morpheme in morphemes_[1:-1]:
+            morphemes[morpheme] += [idx]
+        S[idx] = sgs, ' + '.join(segmented2)
     print(tabulate(table))
+    
+    for morpheme, idxs in sorted(morphemes.items(), key=lambda x: len(x[1])):
+        if len(morpheme) > 1:
+            print('# {0} ({1})'.format(morpheme, len(idxs)))
+            table = []
+            for idx in sorted(idxs, key=lambda x: wordlist[x, 'concept']):
+                table += [[
+                    idx,
+                    wordlist[idx, 'concept'],
+                    S[idx][1]]]
+            print(tabulate(table, headers=['ID', 'Concept', 'Morphemes'],
+                tablefmt='pipe'))
     input()
+
     # write a profile
     return inits, enits, prf
